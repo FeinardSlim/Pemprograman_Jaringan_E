12bf1583b29c15ae39d04d3743c1a32f5f2098c1
FeinardSlim <feinard1234@gmail.com>
2021-06-10 21:45:19 +0700

    init

---
+++ progjar4a/Jawaban/multithread_get.py
@@ -0,0 +1,14 @@
+from file_client_cli import remote_get
+import threading
+
+def download(filename):
+    texec = dict()
+    for i in range(100):
+        texec[i] = threading.Thread(target=remote_get, args=(filename,))
+        texec[i].start()
+    for i in texec:
+        texec[i].join()
+
+if __name__ == '__main__':
+    filename = 'pokijan.jpg'
+    download(filename)
