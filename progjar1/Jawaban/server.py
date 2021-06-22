a3e6cd6d4885ab7860f0d699a48b5ae4d5b92c79
FeinardSlim <feinard1234@gmail.com>
2021-06-22 20:35:33 +0700

    Update server.py

--- progjar1/Jawaban/server.py
+++ progjar1/Jawaban/server.py
@@ -16,26 +16,20 @@ while True:  # Send data
     print(f"connection from {client_address}")
     # Receive the data in small chunks and retransmit it
     while True:
-        data = connection.recv(BUFFER_SIZE)
-        try:
-            command = data.decode().split()
-            print(command)
-            if command[0] == 'file':
-                connection.sendall(('tester_' + command[1]).encode())
-            else:
-                print(f"received {data}")
-                if data:
-                    print("sending back data")
-                    connection.sendall(data)
-                else:
-                   break
-        except:
+        # Wait for a connection
+        print("waiting for a connection")
+        connection, client_address = sock.accept()
+        print(f"connection from {client_address}")
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(32)
             print(f"received {data}")
             if data:
                 print("sending back data")
                 connection.sendall(data)
             else:
+                # print >>sys.stderr, 'no more data from', client_address
+                # print(f"no more data from {client_address}")
                 break
-
-    # Clean up the connection
-    connection.close()
+        # Clean up the connection
+        connection.close()
